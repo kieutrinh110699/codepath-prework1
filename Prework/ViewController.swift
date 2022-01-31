8a4667edbeef11eb11cb40c2ef93849ecab454b1
kieutrinh110699 <94660865+kieutrinh110699@users.noreply.github.com>
2022-01-31 17:18:50 -0600

    Added prework files to repository

---
+++ Prework/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by Trinh Nguyen on 1/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var TextLabel: UILabel!
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("Hello")
+        TextLabel.textColor = UIColor.orange
+    }
+    
+}
+
